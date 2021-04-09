0f591c54adc9ebdae27ae4cffadfbc38c6fcb0ad
a-harshbarger <anne.harshbarger@duke.edu>
2021-04-09 12:42:55 -0400

    Added code to set waves to months, created summary dataset with one datapoint for month-year-combo, created new time series from summary dataset

--- EM.practice.script.R
+++ EM.practice.script.R
@@ -8,10 +8,38 @@ library(tidyr)
 
 fish.dat <- readxl::read_excel("../Data_FinalProject/Data/Raw/mrip_estim_catch_wave_1990_2019_NC.xlsx")
 
+##create function to choose a month for each wave
+wave_to_month_function <-  (function(WAVE) {
+  if(WAVE == 1) {
+    "Jan"
+  }
+  else if(WAVE == 2) {
+    "Mar"
+  }
+  else if(WAVE == 3) {
+    "May"
+  }
+  else if(WAVE == 4) {
+    "Jul"
+  }
+  else if(WAVE == 5) {
+    "Sep"
+  }
+  else {
+    "Nov"
+  }
+})
+
+#vectorize function
+wave_to_month_function_V <- Vectorize(wave_to_month_function)
+
+#create tidy dataset
 fish.tidy <- fish.dat %>%
   select(YEAR, WAVE, MODE_FX, AREA_X, TOT_CAT) %>%
-  mutate(DATE = my(paste0(WAVE, "-", YEAR))) %>%
-  select(DATE, MODE_FX, AREA_X, TOT_CAT)
+  mutate(MONTH = wave_to_month_function_V(WAVE)) %>%
+  mutate(DATE = my(paste0(MONTH, "-", YEAR))) %>%
+  #mutate(DATE = my(paste0(WAVE, "-", YEAR))) %>% # DATE line from Eva's initial draft
+  select(DATE, MONTH, YEAR, MODE_FX, AREA_X, TOT_CAT)
 #wave 1 = Jan 1, wave 2 = March 1, etc --> how do we translate this? for loop? if wave == 1, date = Jan 1
 #on ts fxn, say start = 1st year, 1st wave, then when specifying freq = 6 it should work
 #can use ggplot to make sure it's doing this correctly
@@ -30,7 +58,7 @@ fmonth <- month(first(fish.tidy.filled$DATE))
 fyear <- year(first(fish.tidy.filled$DATE))
 
 fish.monthly.ts <- ts(fish.tidy.filled$TOT_CAT, 
-                             start = c(fyear, fmonth),frequency = 12)
+                             start = c(1990, 1),frequency = 6)
 #change to frequency = 6
 
 month.decomp <- stl(fish.monthly.ts, s.window = "periodic")
@@ -46,4 +74,20 @@ plot(month.decomp)
 #helps with visualizing if the TS is running correctly
 #maybe just copy the wave data so it corresponds to jan and feb etc
 
+#Annie's notes 4/9
+#after adding month corresponding to beginning of wave, still looks funky
+#attempted to fix this by adding sum of TOT_CAT for each wave
+#need to verify if this is ok/why there are so many different point for each
+#possibly due to different combinations of areas, zones of fishing, etc
+fish.tidy.summary <- fish.tidy %>%
+  select(DATE, MONTH, YEAR, TOT_CAT) %>%
+  group_by(DATE) %>%
+  summarise(TOT_CAT_ALL = sum(TOT_CAT))
+  
+#does not have NAs but does have missing observations
+#approximate these later (just testing ts again for now)
+fish.tidy.summary.ts <- ts(fish.tidy.summary$TOT_CAT_ALL, 
+                           start = c(1990, 1),frequency = 6)
+summary.ts.decomp <- stl(fish.tidy.summary.ts, s.window = "periodic")
 
+plot(summary.ts.decomp)
